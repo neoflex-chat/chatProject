4eb3154ca9132e4ded2f01e0a9bbdaf7939bcd9c
Evseev Daniil <devseev@neoflex.ru>
2021-12-25 19:33:47 +0300

    delete files

--- src/store/index.tsx
+++
@@ -1,8 +0,0 @@
-import {types} from 'mobx-state-tree'
-import itemsStore from "./items";
-const RootStore = types
-    .model('RootStore', {
-        items: types.optional(itemsStore,{})
-    });
-
-export default RootStore
